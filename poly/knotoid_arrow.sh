0a2c0daff040dcc3236c6dab746a86594cc9ceeb
biswar00p Maiti <m.biswa.roop@gmail.com>
2025-04-24 07:46:07 +0000

    knotoid scripts

---
+++ poly/knotoid_arrow.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INPUT_DIR="not_e2e_xyzfiles"
+OUTPUT_FILE="arrow_knotoid_polynomial_outputs.txt"
+EXECUTABLE="./src/polynomial_invariant"
+#PROJECTION="1,0,0"
+# Check if a closure method was passed
+if [ -z "$1" ]; then
+  echo "Usage: $0 <closure-method> (e.g., open, direct, rays)"
+  exit 1
+fi
+
+CLOSURE_METHOD="$1"
+
+
+# Empty the output file if it exists
+> "$OUTPUT_FILE"
+
+# Loop over all .xyz files in the input directory
+for file in "$INPUT_DIR"/*.xyz; do
+    filename=$(basename "$file")
+    echo "===== $filename =====" >> "$OUTPUT_FILE"
+    $EXECUTABLE  --closure-method="$CLOSURE_METHOD" --arrow-polynomial "$file" >> "$OUTPUT_FILE"
+    echo -e "\n" >> "$OUTPUT_FILE"
+done
+
+echo "All outputs written to $OUTPUT_FILE"
